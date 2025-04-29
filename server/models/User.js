34a028d440ac3c40ae882130774480cccd25db91
Avinash Guleria <avinashguleria1009@gmail.com>
2025-04-29 17:53:29 +0530

    database schema created

--- server/models/User.js
+++ server/models/User.js
@@ -1,34 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Name is required"]
+      type: String,
+      required: [true, "Name is required"],
     },
     email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: true
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
     },
     password: {
-        type: String,
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: function (value) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/.test(value);
+        },
+        message:
+          "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.",
+      },
     },
     age: {
-        type: Number,
-        required: [true, "Age is required"]
+      type: Number,
+      required: [true, "Age is required"],
     },
     profession: {
-        type: String,
-        required: [true, "Profession is required"]
+      type: String,
+      required: [true, "Profession is required"],
     },
     isGoogleAccount: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     isVerified: {
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true });
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
