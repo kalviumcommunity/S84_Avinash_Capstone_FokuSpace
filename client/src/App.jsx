9ed2c5e4a0b99dfca38de2c1f95f8fb2d6ccd0bb
Avinash Guleria <avinashguleria1009@gmail.com>
2025-05-08 14:59:16 +0530

    changes in components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,41 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Login from "./components/Login";
 import Register from "./components/Register";
+import VerifyOtp from "./components/OtpVerification";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { PrivateRoute } from "./components/PrivateRoute";
 
-// import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/register">Register</Link> |{" "}
+        <Link to="/login">Login</Link> | <Link to="/profile">Profile</Link>
+      </nav>
+      <GoogleLogin
+          onSuccess={credentialResponse => {
+            const token = credentialResponse.credential;
+            // Send token to backend for verification + login
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+
       <Routes>
         <Route path="/" element={<h1>Welcome to FokuSpace</h1>} />
         <Route path="/register" element={<Register />} />
+        <Route path="/verify-otp" element={<VerifyOtp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
