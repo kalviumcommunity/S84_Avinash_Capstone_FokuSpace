23234aa6c82611af79435443cb72853cae4877cd
Avinash Guleria <avinashguleria1009@gmail.com>
2025-06-23 15:48:41 +0530

    cors changes

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(passport.initialize());
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  "process.env.CLIENT_URL",
+  process.env.CLIENT_URL,
 ];
 
 app.use(
