43b368936e376426eb5850aebde322412c3717c4
Avinash Guleria <avinashguleria1009@gmail.com>
2025-07-14 11:47:29 +0530

    done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../styles/home.css";
+import "../styles/Home.css";
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
